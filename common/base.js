dbf4c528e3efa29714ebe5a3c1f1dba6dc16bee9
sesili lomadze <sesili.lomadze.1@btu.edu.ge>
2022-05-13 15:53:57 +0400

    initial commit

---
+++ common/base.js
@@ -0,0 +1,8 @@
+export class base{
+    setId(id){
+        this.id = id;
+    }
+    setHtmlContent(content){
+        this.id.innerHTML=content
+    }
+}
---
+++ index.js
@@ -0,0 +1,8 @@
+import {addItem} from "scripts.js"
+let searchItems=["Internet Cats", "Meme's", "Typing", "Space", "Rick and Morty"];
+
+let serachItemsID = document.getElementById("searchItems")
+let serchItemsBlock = new addItem()
+searchItems.setItemList = searchItems
+searchItems.id = serachItemsID
+serchItemsBlock.render()
